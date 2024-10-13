62aa994c2ac159c74b1d6c97d54dff5a75d1f162
endijsbertans <endijsbertans@gmail.com>
2024-10-13 12:40:56 +0300

    tickets saved using viewchild

--- src/app/dashboard/server-status-component/server-status-component.component.ts
+++ src/app/dashboard/server-status-component/server-status-component.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import {Component, OnDestroy, OnInit} from '@angular/core';
 @Component({
   selector: 'app-server-status-component',
   standalone: true,
@@ -7,18 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './server-status-component.component.html',
   styleUrl: './server-status-component.component.css'
 })
-export class ServerStatusComponentComponent {
-  currentStatus: 'online' | 'offline'| 'unknown' = 'online';
-  constructor() {
-    setInterval(() => {
+export class ServerStatusComponentComponent implements OnInit, OnDestroy{
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private interval?: ReturnType<typeof setInterval>;
+  ngOnInit() {
+    this.interval = setInterval(() => {
       const rnd = Math.random(); // 0 - 0.999999999
-      if(rnd < 0.5) {
+      if (rnd < 0.5) {
         this.currentStatus = 'online';
-      }else if(rnd < 0.9){
+      } else if (rnd < 0.9) {
         this.currentStatus = 'offline';
-      }else{
+      } else {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+  }
+  ngOnDestroy() {
+    clearTimeout(this.interval);
   }
 }
--- src/app/dashboard/support-ticket-component/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/support-ticket-component/new-ticket/new-ticket.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, viewChild, ViewChild} from '@angular/core';
 import {ButtonComponent} from "../../../shared/button/button.component";
 import {ControlComponent} from "../../../shared/control/control.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   imports: [
     ButtonComponent,
-    ControlComponent
+    ControlComponent,
+    FormsModule
   ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  //                        .required
+  //private form = viewChild<ElementRef<HTMLFormElement>>(ButtonComponent); //SIGNALSSS
+  onSubmit(title: string, ticketText: string) {
 
+    console.log(title);
+    console.log(ticketText);
+    //       ()?
+    this.form?.nativeElement.reset();
+  }
 }
